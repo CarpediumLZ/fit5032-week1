0ba51416415a898b705a699c993905f983958dd2
capersoul <78712491+capersoul@users.noreply.github.com>
2026-08-07 01:29:55 +1000

    initial commit

---
+++ src/components/CongQin.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Hello, my name is Cong Qin!</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import CongQin from './components/CongQin.vue'
+
+createApp(App).mount('#app')
