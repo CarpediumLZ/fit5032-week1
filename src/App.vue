5485d176707c65bdd38b6ecde62f7977bb145d94
capersoul <78712491+capersoul@users.noreply.github.com>
2026-08-21 22:51:15 +1000

    week 3 & 4 eFolio 3.1 and 4.1 complete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import JSONLab from "./components/JSON.vue";
 import CongQin from './components/CongQin.vue'
+import Form from "./components/LibraryRegistrationForm.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <CongQin />
-  </main>
+  <CongQin />
+  <JSONLab />
+  <Form />
 </template>
 
 <style scoped>
